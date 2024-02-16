6ceccddc6ff216dda8fb54e2336f537cad37c110
Nana <1986938126@qq.com>
2024-02-16 23:57:16 +0800

    初始化

---
+++ src/main/java/com/example/enterprisecrm/EnterpriseCrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.enterprisecrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseCrmApplication.class, args);
+    }
+
+}
